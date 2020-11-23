3c049d72ff928d1bb381fd899794e17853d2099e
Olivier LDff <olivier.ldff@gmail.com>
2020-11-23 12:49:02 +0100

    ♻️ Refactor for v1.4.0 🚚 Merge include & src folder 🚚 Merge res and qml folder 🔨 Introduce intermediate library QaterialComponents 📄 Update license in some files

--- include/Qaterial/Qaterial.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef __QATERIAL_HPP__
-#define __QATERIAL_HPP__
-
-#include <Qaterial/Export.hpp>
-#include <Qaterial/Version.hpp>
-#include <Qaterial/Utils.hpp>
-#include <Qaterial/Logger.hpp>
-
-#endif
