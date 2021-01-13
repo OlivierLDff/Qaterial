606848387ec95b001106ce93e599d80975258bf9
tonghao.yuan <tonghao.yuan@csdental.com>
2021-01-13 12:35:12 +0800

    :tada: add an option to build examples

---
+++ examples/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,30 @@
+#include <Qaterial/Qaterial.hpp>
+
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+
+int main(int argc, char* argv[])
+{
+    QGuiApplication app(argc, argv);
+    QQmlApplicationEngine engine;
+
+    // Important to find 'import Qaterial 1.0' in qml
+    // Qaterial module is located in qrc:Qaterial/
+    engine.addImportPath("qrc:///");
+
+    // Load Qaterial.
+    qaterial::loadQmlResources();
+    qaterial::registerQmlTypes();
+
+    // Load QaterialHelloWorld
+    Q_INIT_RESOURCE(HelloWorld);
+
+   	// This path is defined by the prefix in HelloWorld.qrc
+    engine.load(QUrl("qrc:/HelloWorld.qml"));
+    if(engine.rootObjects().isEmpty())
+        return -1;
+
+    // Start event loop
+    return QGuiApplication::exec();
+}
---
+++ examples/HelloWorld/HelloWorld.qml
@@ -0,0 +1,14 @@
+import Qaterial 1.0 as Qaterial
+
+Qaterial.ApplicationWindow
+{
+  width: 400
+  height: 200
+  visible: true
+
+  Qaterial.Label
+  {
+    text: 'Hello World'
+    anchors.centerIn: parent
+  }
+}
