0944998e411b5045b8d7f2086c21147cba66ee05
Olivier LDff <olivier.ldff@gmail.com>
2021-07-15 12:48:39 +0200

    ♻️ Use qmlRegisterSingletonInstance instead of qmlRegisterSingletonType This give a more explicit idea of what is happening

--- src/Qaterial/Details/Property.hpp
+++ src/Qaterial/Details/Property.hpp
@@ -362,20 +362,12 @@ public:                                                                         
         static Class ret;                                                                                                                  \
         return ret;                                                                                                                        \
     }                                                                                                                                      \
-    static QObject* set##Name(QQmlEngine* qmlEngine, QJSEngine* jsEngine)                                                                  \
-    {                                                                                                                                      \
-        Q_UNUSED(jsEngine)                                                                                                                 \
-        Q_UNUSED(qmlEngine)                                                                                                                \
-        QObject* ret = &name();                                                                                                            \
-        QQmlEngine::setObjectOwnership(ret, QQmlEngine::CppOwnership);                                                                     \
-        return ret;                                                                                                                        \
-    }                                                                                                                                      \
     static void registerSingleton(const char* uri = "Qaterial",                                                                            \
         const int majorVersion = 1,                                                                                                        \
         const int minorVersion = 0,                                                                                                        \
         const char* n = #Class)                                                                                                            \
     {                                                                                                                                      \
-        qmlRegisterSingletonType<Class>(uri, majorVersion, minorVersion, n, &Class::set##Name);                                            \
+        qmlRegisterSingletonInstance(uri, majorVersion, minorVersion, n, &name());                                                         \
     }                                                                                                                                      \
                                                                                                                                            \
 private:
