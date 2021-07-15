dde1823f63ff68052ba5efbb8fda55a45c7d8b09
Olivier LDff <olivier.ldff@gmail.com>
2021-07-15 10:28:18 +0200

    📌 Pin google font repository tag

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -12,7 +12,7 @@ set(FETCHGOOGLEFONTCMAKE_REPOSITORY "https://github.com/OlivierLDff/FetchGoogleF
 set(FETCHGOOGLEFONTCMAKE_TAG "c19d83a68eda83b9dd3033893dbb63d84c941196" CACHE STRING "FetchGoogleFontCMake git tag")
 
 set(GOOGLEFONTS_REPOSITORY "https://github.com/OlivierLDff/fonts" CACHE STRING "Google fonts repository, can be a local URL")
-set(GOOGLEFONTS_TAG "master" CACHE STRING "Google fonts git tag")
+set(GOOGLEFONTS_TAG "0cdadf80ac5f29f10a73c29ce1f4f1ca75ffb392" CACHE STRING "Google fonts git tag")
 
 ## C++ libraries
 
