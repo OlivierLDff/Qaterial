5286cfc421e9b2ba89e3074f9d2ca34495cba411
Olivier LDff <olivier.ldff@gmail.com>
2021-07-15 15:58:39 +0200

    🔨 QATERIAL_ENABLE_INSTALL - Support installation with find_package - auto find_package Qt

--- cmake/FetchQOlm.cmake
+++ cmake/FetchQOlm.cmake
@@ -14,4 +14,5 @@ FetchContent_Declare(
 )
 
 set(QOLM_FOLDER_PREFIX "Dependencies" CACHE STRING "Prefix folder for all QOlm generated targets in generated project (only decorative)")
+set(QOLM_ENABLE_INSTALL ${QATERIAL_ENABLE_INSTALL} CACHE BOOL "Generate the install target")
 FetchContent_MakeAvailable(QOlm)
