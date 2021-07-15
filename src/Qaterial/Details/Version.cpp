34d242ace38d782d7e0aa204a243717463aaf763
Olivier LDff <olivier.ldff@gmail.com>
2021-07-15 12:32:17 +0200

    ♻️ Version only use static method

--- src/Qaterial/Details/Version.cpp
+++ src/Qaterial/Details/Version.cpp
@@ -26,11 +26,35 @@ using namespace qaterial;
 
 Version::Version(QObject* parent)
     : QObject(parent)
-    , _major(QATERIAL_VERSION_MAJOR)
-    , _minor(QATERIAL_VERSION_MINOR)
-    , _patch(QATERIAL_VERSION_PATCH)
-    , _tag(QATERIAL_VERSION_TAG_HEX)
-    , _readable(QString::number(_major) + QStringLiteral(".") + QString::number(_minor) + QStringLiteral(".") + QString::number(_patch)
-                + QStringLiteral(".0x") + QString::number(_tag, 16).rightJustified(8, QChar('0')))
 {
+}
+
+quint32 Version::major()
+{
+    return QATERIAL_VERSION_MAJOR;
+}
+
+quint32 Version::minor()
+{
+    return QATERIAL_VERSION_MINOR;
+}
+
+quint32 Version::patch()
+{
+    return QATERIAL_VERSION_PATCH;
+}
+
+quint32 Version::tag()
+{
+    return QATERIAL_VERSION_TAG_HEX;
+}
+
+QString Version::readable()
+{
+    return QString::number(major()) + QStringLiteral(".") + QString::number(minor()) + QStringLiteral(".") + QString::number(patch());
+}
+
+QString Version::readableWithTag()
+{
+    return readable() + QStringLiteral(".0x") + QString::number(tag(), 16).rightJustified(8, QChar('0'));
 }
--- src/Qaterial/Details/Version.hpp
+++ src/Qaterial/Details/Version.hpp
@@ -47,35 +47,15 @@ private:
     Q_PROPERTY(quint32 patch READ patch CONSTANT);
     Q_PROPERTY(quint32 tag READ tag CONSTANT);
     Q_PROPERTY(QString readable READ readable CONSTANT);
-
-private:
-    quint32 _major = 0;
-    quint32 _minor = 0;
-    quint32 _patch = 0;
-    quint32 _tag = 0;
-    QString _readable;
+    Q_PROPERTY(QString readableWithTag READ readableWithTag CONSTANT);
 
 public:
-    quint32 major() const
-    {
-        return _major;
-    }
-    quint32 minor() const
-    {
-        return _minor;
-    }
-    quint32 patch() const
-    {
-        return _patch;
-    }
-    quint32 tag() const
-    {
-        return _tag;
-    }
-    QString readable() const
-    {
-        return _readable;
-    }
+    static quint32 major();
+    static quint32 minor();
+    static quint32 patch();
+    static quint32 tag();
+    static QString readable();
+    static QString readableWithTag();
 };
 
 }
