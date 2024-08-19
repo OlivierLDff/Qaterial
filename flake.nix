39bb7f02fa3b789d8605be545e8587ac149d2945
Olivier Le Doeuff <olivier.ldff@gmail.com>
2024-08-19 09:16:21 +0200

    add flake

---
+++ flake.nix
@@ -0,0 +1,127 @@
+# SPDX-FileCopyrightText: Naostage <contact@naostage.com>
+# SPDX-License-Identifier: UNLICENSED
+{
+  description = "qaterial";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+
+      qt = pkgs.qt6;
+
+      nativeBuildInputs = with pkgs; [
+        qt.wrapQtAppsHook
+        makeWrapper
+        gcc
+        git
+        cmake
+        cpm-cmake
+        ninja
+
+        gtest
+      ];
+
+      buildInputs = with pkgs; [
+        qt.qtbase
+        qt.qtsvg
+        qt.qtdeclarative
+        qt.qtquickcontrols2
+      ];
+
+      nativeCheckInputs = with pkgs; [
+        dbus
+        xvfb-run
+      ];
+
+      shellHook = ''
+        # Crazy shell hook to set up Qt environment, from:
+        # https://discourse.nixos.org/t/python-qt-woes/11808/12
+        setQtEnvironment=$(mktemp --suffix .setQtEnvironment.sh)
+        echo "shellHook: setQtEnvironment = $setQtEnvironment"
+        makeWrapper "/bin/sh" "$setQtEnvironment" "''${qtWrapperArgs[@]}"
+        sed "/^exec/d" -i "$setQtEnvironment"
+        source "$setQtEnvironment"
+      '';
+
+      devShellHook = pkgs.lib.concatStringsSep "\n" (
+        [ shellHook ]
+      );
+
+
+      CPM_USE_LOCAL_PACKAGES = "ON";
+
+      packages = {
+        default = null;
+        deadnix = pkgs.runCommand "deadnix" { } ''
+          ${pkgs.deadnix}/bin/deadnix --fail ${./.}
+          mkdir $out
+        '';
+      };
+
+      apps = {
+        default = null;
+      };
+
+      minimalDevBuildInputs = with pkgs; [
+        gh
+      ];
+      fullDevBuildInputs = with pkgs; nativeBuildInputs
+        ++ nativeCheckInputs
+        ++ minimalDevBuildInputs
+        ++ [
+        sccache
+        nixpkgs-fmt
+        cmake-format
+        clang-tools
+        lazygit
+        neovim
+      ]
+        ++ pkgs.lib.lists.optionals (stdenv.isLinux) [
+        gdb
+      ];
+
+    in
+    {
+      inherit packages apps;
+
+      devShells = {
+        minimal = pkgs.mkShell {
+          name = "qaterial-minimal-shell";
+
+          inherit buildInputs shellHook;
+          inherit CPM_USE_LOCAL_PACKAGES;
+
+          nativeBuildInputs = nativeBuildInputs
+            ++ nativeCheckInputs
+            ++ minimalDevBuildInputs;
+        };
+
+        default = pkgs.mkShell {
+          name = "qaterial-dev-shell";
+
+          inherit buildInputs;
+          inherit CPM_USE_LOCAL_PACKAGES;
+
+          shellHook = devShellHook;
+          nativeBuildInputs = fullDevBuildInputs;
+        };
+      };
+
+      formatter = pkgs.nixpkgs-fmt;
+      checks = {
+        inherit (self.packages.${system}) deadnix;
+      };
+    });
+}
